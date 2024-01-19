05b3699e85e221f60542ac7519f86182acd19bbc
11yhj <uouo_@naver.com>
2024-01-19 10:18:49 +0900

    현주 카카오로딩페이지 작업중

---
+++ src/component/Kakao/Loading.jsx
@@ -0,0 +1,53 @@
+import { styled } from "styled-components";
+import logo from "../../images/cinepic_logo.png";
+
+const LoadingComp = styled.section`
+  width: 100;
+  height: 80vh;
+  .container {
+    display: flex;
+    align-items: center;
+    padding-top: 100px;
+    .logo {
+      width: 150px;
+      margin-right: 20px;
+    }
+    h2 {
+      font-size: 1.8;
+      font-weight: 600;
+      text-align: center;
+      color: var(--BLACK);
+    }
+  }
+  @media only screen and (max-width: 480px) {
+    .container {
+      flex-direction: column;
+      justify-content: center;
+      .logo {
+        width: 100px;
+        margin-right: 0;
+        margin-bottom: 40px;
+      }
+      h2 {
+        font-size: 1.4em;
+      }
+    }
+  }
+`;
+
+const Loading = () => {
+  return (
+    <>
+      <LoadingComp>
+        <div className="container">
+          <img className="logo" src={logo} alt="logo" />
+          <div className="LoadingBox">
+            <h2>로그인 중입니다 잠시만 기다려 주세요</h2>
+          </div>
+        </div>
+      </LoadingComp>
+    </>
+  );
+};
+
+export default Loading;
---
+++ src/component/MyPage/MyInfo.jsx
@@ -0,0 +1,9 @@
+import { styled } from "styled-components";
+import { useNavigate } from "react-router-dom";
+import Button from "../../util/Button";
+
+const MyInfoComp = styled.section`
+  width: 100%;
+  padding-top: 80px;
+  margin-bottom: 100px;
+`;
