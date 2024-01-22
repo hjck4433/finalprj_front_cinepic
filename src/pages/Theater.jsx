d479ab43091b57081c41d13a3a80888efeb2dc2a
ghkdtjsdud0312 <142463073+ghkdtjsdud0312@users.noreply.github.com>
2024-01-22 16:39:45 +0900

    선영 지도 마커에 맞게 위치이동, 마커누르면 상세정보나오고 이동, 초기값나오고 없어짐 추가

--- src/pages/Theater.jsx
+++ src/pages/Theater.jsx
@@ -2,74 +2,45 @@ import React, { useEffect, useRef, useState } from "react";
 import TheaterComp from "../component/Theater/theaterStyles";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
-import { useKakaoLoader } from "react-kakao-maps-sdk";
-import { Map } from "react-kakao-maps-sdk";
+import TheaterApi from "../api/TheaterApi";
 
 const Theater = () => {
   const mapRef = useRef(null); // 지도 링크
-  const [location, setLocation] = useState({ lat: 0, long: 0 }); // 위도, 경도
+  const [location, setLocation] = useState({
+    lat: 37.5139545,
+    long: 127.1049251,
+  }); // 위도, 경도
   const [searchQuery, setSearchQuery] = useState(""); // 검색어
   const [map, setMap] = useState(null); // 지도
-  const [text, setText] = useState("");
-  const [theaterData, setTheaterData] = useState("");
-  const [selectedPlace, setSelectedPlace] = useState(null);
-  const [markers, setMarkers] = useState([]);
-  const [state, setState] = useState({
-    // 지도 초기 위치
-    center: { lat: 37.5139545, long: 127.1049251 },
-    // 지도 위치 변경시 panto를 이용할 지에 대해서 정의
-    isPanto: false,
-  });
-
-  // 더미데이터 생성 해보기
-  const [list, setList] = useState([
-    {
-      id: "1224",
-      province: "서울시",
-      city: "송파구",
-      theaterName: "롯데시네마 월드타워",
-      theaterAddr:
-        "서울특별시 송파구 신천동 29번지 신천동, 엔터동롯데시네마(5~11층)",
-      theaterPhone: "1544-8855",
-      seats: "4,204",
-      screens: 18,
-      isSpecialScreen: "있음",
-      screenFilm: 0,
-      screen2D: 18,
-      screen3D: 6,
-      screen4D: 1,
-      screenImax: 1,
-      latitude: "37.5139545",
-      longitude: "127.1049251",
-    },
-  ]);
+  const [theaterData, setTheaterData] = useState([]); // 극장 정보 불러오기
+  const [selectedPlace, setSelectedPlace] = useState(null); // 선택된 장소 데이터 불러오기
+  const [markers, setMarkers] = useState([]); // 마커 배열
 
   // 지도 불러오기
   useEffect(() => {
     const container = mapRef.current; // 지도를 담을 영역의 DOM 레퍼런스
     const options = {
-      center: new window.kakao.maps.LatLng(37.5139545, 127.1049251),
+      center: new window.kakao.maps.LatLng(location.lat, location.long),
       level: 5,
-      isPanto: false,
     };
 
     const kakaoMap = new window.kakao.maps.Map(container, options);
     setMap(kakaoMap);
-  }, [location]);
 
-  // 초기 정보 위치 값 마커로 보여줌
-  const iwPosition = new window.kakao.maps.LatLng(37.5139545, 127.1049251);
-  const infowindow = new window.kakao.maps.InfoWindow({
-    position: iwPosition,
-  });
-  infowindow.open();
-  // Marker 생성 및 지도에 추가
-  const marker = new window.kakao.maps.Marker({
-    position: iwPosition,
-  });
-
-  // 지도에 마커 추가
-  marker.setMap(map);
+    // 서버에서 초기값을 불러오는 비동기 함수
+    const fetchInitialData = async () => {
+      try {
+        const resp = await TheaterApi.theaterIdDetail();
+        setTheaterData([resp.data]);
+        console.log("초기 데이터 : " + resp.data.longitude);
+        setSelectedPlace(resp.data);
+        setLocation({ lat: resp.data.latitude, long: resp.data.longitude });
+      } catch (error) {
+        console.error("초기 값 정보가 아님:", error);
+      }
+    };
+    fetchInitialData();
+  }, []);
 
   // 검색 상자
   const handleSearchInputChange = (event) => {
@@ -77,83 +48,50 @@ const Theater = () => {
     setSearchQuery(event.target.value);
   };
 
-  // 검색 확인 버튼 누를 시 내용 조회(더미데이터 때 사용)
+  // 검색 확인 버튼 누를 시 내용 조회
   const handleSearchButtonClick = async () => {
-    const query = searchQuery.trim();
-
-    if (query) {
-      // 검색어에 해당하는 데이터 찾기
-      const searchData = list.find(
-        (item) => item.province.includes(query) || item.city.includes(query)
-      );
-
-      if (searchData) {
-        // 검색어에 해당하는 데이터가 있으면 해당 위치에 마커 추가
-        const marker = new window.kakao.maps.Marker({
-          position: new window.kakao.maps.LatLng(
-            searchData.latitude,
-            searchData.longitude
-          ),
-        });
-
-        marker.setMap(map);
-
-        // 기존 마커들 제거
-        markers.forEach((m) => m.setMap(null));
-
-        // 새로운 마커 추가
-        setMarkers([marker]);
-
-        // 선택된 장소 업데이트
-        setSelectedPlace(searchData);
-      }
+    console.log("검색 키워드: ", searchQuery);
+    const resp = await TheaterApi.theaterAddrSearch(searchQuery);
+    if (resp.data !== null) {
+      setTheaterData(resp.data);
+      console.log("지도정보 : " + resp.data);
+      setSearchQuery(""); // 엔터 시 글 초기화
+      setLocation({ lat: resp.data[0].latitude, long: resp.data[0].longitude });
     }
-    // 검색 완료 후 검색어 초기화
-    setSearchQuery("");
   };
 
-  // 검색 확인 버튼 누를 시 내용 조회
-  // const handleSearchButtonClick = async () => {
-  // console.log("검색 : ", searchQuery);
-  // setText(searchQuery);
-  // const resp = await Api.,mapSearch(searchQuery);
-  // setTheaterData(resp.data);
-  // console.log(resp);
-  // setSearchQuery(""); // 엔터 시 글 초기화
-  // };
-
   // 지도에 마커 표시하기
-  // useEffect(() => {
-  //   if (!theaterData) return;
-  //   markers.forEach((marker) => marker.setMap(null));
+  useEffect(() => {
+    if (!theaterData) return;
+    markers.forEach((marker) => marker.setMap(null));
 
-  //   const newMarkers = theaterData.map((place) => {
-  //     const placeMarker = new window.kakao.maps.Marker({
-  //       position: new window.kakao.maps.LatLng(place.latitude, place.longitude),
-  //     });
-  //     placeMarker.setMap(map); // 지도에 마커 표시
-  //     window.kakao.maps.event.addListener(placeMarker, "click", () => {
-  //       setSelectedPlace(place);
-  //     });
-  //     return placeMarker;
-  //   });
-  //   setMarkers(newMarkers);
-  // }, [theaterData]);
+    const newMarkers = theaterData.map((place) => {
+      const placeMarker = new window.kakao.maps.Marker({
+        position: new window.kakao.maps.LatLng(place.latitude, place.longitude),
+      });
+      placeMarker.setMap(map); // 지도에 마커 표시
+      window.kakao.maps.event.addListener(placeMarker, "click", () => {
+        setSelectedPlace(place);
+
+        map.setCenter(
+          new window.kakao.maps.LatLng(place.latitude, place.longitude)
+        );
+      });
+      return placeMarker;
+    });
+    setMarkers(newMarkers);
+
+    if (theaterData.length > 0) {
+      const firstPlace = theaterData[0];
+      map.setCenter(
+        new window.kakao.maps.LatLng(firstPlace.latitude, firstPlace.longitude)
+      );
+    }
+  }, [theaterData, map]);
 
   return (
     <>
       <TheaterComp>
-        <Map center={state.center} isPanto={state.isPanto}>
-          <button
-            onClick={() =>
-              setState({
-                center: { lat: state.lat, long: state.long },
-                isPanto: true,
-              })
-            }>
-            {/* 지도 중심좌표 부드럽게 이동시키기 */}
-          </button>
-        </Map>
         <div className="container">
           <h2>인디스페이스 - 영화예술 상영관 검색</h2>
           <div className="mapContainer" ref={mapRef}></div>
@@ -179,67 +117,66 @@ const Theater = () => {
               </div>
             </div>
           </div>
-          {list &&
-            list.map((list) => (
-              <div className="infoContainer" key={list.id}>
-                {/* CGV 청주(서문) */}
-                <h3>{list.theaterName}</h3>
-                <div className="basicInfo1">
-                  <h3>| 기본정보</h3>
-                  <div className="content1">
-                    <div>
-                      <span>상영관 </span>
-                      {list.theaterName}
-                    </div>
-                    <div>
-                      <span>주소 </span>
-                      {list.theaterAddr}
-                    </div>
-                    <div>
-                      <span>전화번호 </span>
-                      {list.theaterPhone}
-                    </div>
-                    <div>
-                      <span>총 좌석 수 </span>
-                      {list.seats}석
-                    </div>
+          {selectedPlace && selectedPlace !== null && (
+            <div className="infoContainer" key={selectedPlace.id}>
+              {/* CGV 청주(서문) */}
+              <h3>{selectedPlace.theaterName}</h3>
+              <div className="basicInfo1">
+                <h3>| 기본정보</h3>
+                <div className="content1">
+                  <div>
+                    <span>상영관 </span>
+                    {selectedPlace.theaterName}
                   </div>
-                </div>
-                <div className="basicInfo2">
-                  <h3>| 스크린관 정보</h3>
-                  <div className="content2">
-                    <div>
-                      <span>총 스크린 수 </span>
-                      {list.screens}개
-                    </div>
-                    <div>
-                      <span>특별관 운영 여부 </span>
-                      {list.isSpecialScreen}
-                    </div>
-                    <div>
-                      <span>필름 상영관 수 </span>
-                      {list.screenFilm}개
-                    </div>
-                    <div>
-                      <span> 2D 상영관 수 </span>
-                      {list.screen2D}개
-                    </div>
-                    <div>
-                      <span>3D 상영관 수 </span>
-                      {list.screen3D}개
-                    </div>
-                    <div>
-                      <span>4D 상영관 수 </span>
-                      {list.screen4D}개
-                    </div>
-                    <div>
-                      <span> IMAX 상영관 수 </span>
-                      {list.screenImax}개
-                    </div>
+                  <div>
+                    <span>주소 </span>
+                    {selectedPlace.theaterAddr}
+                  </div>
+                  <div>
+                    <span>전화번호 </span>
+                    {selectedPlace.theaterPhone}
+                  </div>
+                  <div>
+                    <span>총 좌석 수 </span>
+                    {selectedPlace.seats}석
                   </div>
                 </div>
               </div>
-            ))}
+              <div className="basicInfo2">
+                <h3>| 스크린관 정보</h3>
+                <div className="content2">
+                  <div>
+                    <span>총 스크린 수 </span>
+                    {selectedPlace.screens}개
+                  </div>
+                  <div>
+                    <span>특별관 운영 여부 </span>
+                    {selectedPlace.isSpecialScreen}
+                  </div>
+                  <div>
+                    <span>필름 상영관 수 </span>
+                    {selectedPlace.screenFilm}개
+                  </div>
+                  <div>
+                    <span> 2D 상영관 수 </span>
+                    {selectedPlace.screen2D}개
+                  </div>
+                  <div>
+                    <span>3D 상영관 수 </span>
+                    {selectedPlace.screen3D}개
+                  </div>
+                  <div>
+                    <span>4D 상영관 수 </span>
+                    {selectedPlace.screen4D}개
+                  </div>
+                  <div>
+                    <span> IMAX 상영관 수 </span>
+                    {selectedPlace.screenImax}개
+                  </div>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
       </TheaterComp>
     </>
