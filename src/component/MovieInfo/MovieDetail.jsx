68f8d257387ac70ebbfb17792842973fb36aee3c
lumiereJE <lumiere_remplir@naver.com>
2024-01-22 15:55:37 +0900

    2024/01/22 김지은 movieDetail 반응형 추가

--- src/component/MovieInfo/MovieDetail.jsx
+++ src/component/MovieInfo/MovieDetail.jsx
@@ -1,62 +1,118 @@
 import styled from "styled-components";
-
+import Bookmark from "../MovieSearch/Bookmark";
 
 const MovieDetailComp = styled.section`
-	width: 100vw;
-	height: 500px;
+	/* width: 100vw; */
 	background-color: var(--BLACK);
 	.container {
-		background-color: pink;
+		padding: 5% 0;
 		display: flex;
 		justify-content: center;
 		align-items: center;
     gap: 5%;
 		.img_box {
-			width: 200px;
-			height: calc(200px * 1.48);
+			width: 22%;
+      position: relative;
 		}
 		.text_box {
-			width: 50%;
+			width: 40%;
 			.title_box {
 				font-weight: 600;
 				p:nth-child(1) {
 					font-size: 2.5em;
 					color: #fff;
-					margin-bottom: 3%;
+					margin-bottom: 1%;
 				}
 				p:nth-child(2) {
-					font-size: 1.7em;
+					font-size: 1.5em;
 					color: var(--GREY);
 					margin-bottom: 10%;
 				}
 			}
 			.movie_info {
 				font-size: 1.3em;
-				font-weight: 600;
 				div {
 					font-weight: 600;
 					margin-bottom: 5%;
 				}
 				span:nth-child(1) {
+          width: 30%;
+          color: var(--ORANGE);
 					display: inline-block;
-					width: 20%;
+          font-weight: 900;
 				}
 				span:nth-child(2) {
+          width: 70%;
+          color: var(--GREY);
 					display: inline-block;
 				}
 			}
 		}
-		
 	}
+  @media only screen and (max-width:1200px){
+    .container {
+      
+    }
+  }
+  @media only screen and (max-width:768px) {
+    .container {
+      display: block;
+      padding: 10% 0;
+      .img_box {
+        width: 70%;
+        margin: 0 auto 10%;
+      }
+      .text_box {
+        width: 70%;
+        margin: 0 auto;
+        
+        .title_box{
+          p:nth-child(1) {
+            margin-bottom: 5%;
+          }
+          p:nth-child(2) {
+            margin-bottom: 15%;
+          }
+        }
+        .movie_info {
+          font-size: 1.5em;
+          div {
+            font-size: 300;
+            margin-bottom: 8%;
+          }
+          span:nth-child(1) {
+            margin-right: 5%;
+          }
+          span:nth-child(2) {
+            width: 65%;
+          }
+        }
+      }
+    }  
+  }
+  @media only screen and (max-width: 480px) {
+    padding: 10% 0;
+  .container {
+    padding: 0;
+    .img_box {
+      width: 100%;
+    }
+    .text_box {
+      width: 100%;
+    }
+  }
+
+  }
 `;
 const ImgComp = styled.div`
 	width: 100%;
-	height: 100%;
+	padding-bottom: 144%;
 	background-image: url(${(props) => props.$imgsrc});
 	background-size: 100%;
 	background-position: center;
 	background-repeat: no-repeat;
 `;
+
 
 const MovieDetail = ({data}) => { 
 
@@ -67,7 +123,7 @@ const MovieDetail = ({data}) => {
 				<div className="container">
 					<div className="img_box">
 						<ImgComp $imgsrc={data.moviePoster}/>
-						{/* 북마크 추가해야 됨 */}
+						<Bookmark />
 					</div>
 					<div className="text_box">
 						<div className="title_box">
@@ -97,11 +153,10 @@ const MovieDetail = ({data}) => {
 							</div>
 							<div>
 								<span>상영시간</span>
-								<span>{data.movieRuntime}</span>
+								<span>{data.movieRuntime}분</span>
 							</div>
 						</div>
 					</div>
-	
 				</div>
 			</MovieDetailComp>
 		</>
