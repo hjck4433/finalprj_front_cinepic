bb5bec6c2ef67c0b4dae9a798ceefa4db8f071b1
pshyun123 <pshyun7204@gmail.com>
2024-02-07 15:02:18 +0900

    소현 관리자 멤버 테이블 숫자 자동증가

--- src/pages/Admin/AdminMember.jsx
+++ src/pages/Admin/AdminMember.jsx
@@ -78,35 +78,44 @@ const AdminMemberComp = styled.div`
 `;
 const AdminMember = () => {
   // 페이지네이션 관련
-  const [totalPage, setTotalPage] = useState(5);
-  const [page, setPage] = useState(1);
-  const [memData, setMemData] = useState([]);
-  const [editId, setEditId] = useState("");
+  const [totalPage, setTotalPage] = useState(5); // 전체 페이지 수 상태 초기화
+  const [page, setPage] = useState(1); // 현재 페이지 상태 초기화
+  const [memData, setMemData] = useState([]); // 멤버 데이터 배열 상태 초기화
+  const [editId, setEditId] = useState(""); // 수정할 멤버 ID 상태 초기화
 
   // 멤버정보 불러오기
   const adminMemList = async (page) => {
     const rsp = await MemberApi.memberPage(page);
     if (rsp.data !== null) {
-      setMemData(rsp.data);
+      setMemData(rsp.data); // 가져온 멤버 정보를 상태에 저장
     }
   };
-  const getAdminMemList = useTokenAxios(() => adminMemList(page));
-  const getFirstPage = useTokenAxios(() => adminMemList(1));
+  const getAdminMemList = useTokenAxios(() => adminMemList(page)); // 현재 페이지에 해당하는 멤버정보 가져옴
+  const getFirstPage = useTokenAxios(() => adminMemList(1)); // 첫번째 페이지의 멤버정보 가져옴
 
+  // 페이지 목록 가져오는 함수
   const fetchPageList = async () => {
-    setPage(1);
+    setPage(1); // 첫번째 페이지로 이동
     const res = await MemberApi.getTotalPage();
     if (res.data !== null) {
-      setTotalPage(res.data);
-      getFirstPage();
+      setTotalPage(res.data); // 전체 페이지 수 상태에 저장
+      getFirstPage(); // 첫번째 페이지 멤버정보 가져옴
     }
   };
-  const getTotalPage = useTokenAxios(fetchPageList);
 
+  const getTotalPage = useTokenAxios(fetchPageList); // fetchPageList 사용해 전체 페이지 수 가져오기
+
+  //페이지당 회원 수
+  const itemsPerPage = 10;
+  // 현재 페이지의 첫 번째 항목의 인덱스
+  const startIndex = (page - 1) * itemsPerPage;
+
+  // 페이지 변경시, 해당 페이지의 멤버 정보 가져옴
   useEffect(() => {
     getAdminMemList();
   }, [page]);
 
+  // 컴포넌트가 처음 마운트 될때 전체 페이지 목록 가져옴
   useEffect(() => {
     getTotalPage();
   }, []);
@@ -171,7 +180,7 @@ const AdminMember = () => {
                     <MemTr
                       key={data.email}
                       data={data}
-                      index={index}
+                      index={startIndex + index} // 인덱스 계산
                       setId={setEditId}
                       deleteModal={() => {
                         handleModal("삭제", "삭제하시겠습니까?", true);
